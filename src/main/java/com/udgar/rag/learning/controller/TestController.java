5f3f444d5280b75af531999351231ead7ca4e8b9
udgarkhanal <khanaludgar@gmail.com>
2026-09-25 15:11:35 +0545

    add api documentation

--- src/main/java/com/udgar/rag/learning/controller/TestController.java
+++ src/main/java/com/udgar/rag/learning/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.udgar.rag.learning.controller;
 
 import com.udgar.rag.learning.service.RestClientService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +22,23 @@ public class TestController {
         this.service = service;
     }
 
-    @GetMapping
-    public ResponseEntity<String> testMessage() {
-        String response = service.testService();
-        return ResponseEntity.ok().body(response);
+    @Operation(
+            operationId = "call-llm-rag-service",
+            summary = "Api is invoked to anwer the questions regarding available documentation",
+            description = "Currently we have an Constitution of nepal as a source docs, in this endpoint the users are allowed to ask questions regarding that document and nothing else"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "LLM provides the appropriate answers as mentioned in document",
+                    content = @Content(
+                            mediaType = MediaType.TEXT_PLAIN_VALUE,
+                            schema = @Schema(type = "string")
+                    )
+            )
+    })
+    @PostMapping
+    public String answers(@RequestBody String questions) {
+        return service.answers(questions);
     }
 }
