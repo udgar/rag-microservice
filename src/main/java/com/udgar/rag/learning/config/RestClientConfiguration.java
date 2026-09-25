367531b074576f29ff1d64f0a47d0311a233f033
udgarkhanal <khanaludgar@gmail.com>
2026-09-25 14:54:57 +0545

    add chat client configuration

--- src/main/java/com/udgar/rag/learning/config/RestClientConfiguration.java
+++ src/main/java/com/udgar/rag/learning/config/RestClientConfiguration.java
@@ -1,5 +1,8 @@
 package com.udgar.rag.learning.config;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,15 @@ public class RestClientConfiguration {
     @Bean
     RestClient restClient(@Value("${outbound.llm.request.api}") String baseUrl) {
         return RestClient.builder().baseUrl(baseUrl).build();
+    }
+
+    @Bean
+    ChatClient chatClient(@Value("${outbound.llm.request.api}") String baseUrl, ChatClient.Builder builder, VectorStore store) {
+        return builder
+                .defaultSystem("You are a chat assistant that only answers questions, regarding the information stored in vector store," +
+                        "if asked for anything else deny the request ")
+                .defaultAdvisors(QuestionAnswerAdvisor.builder(store).build())
+                .build();
     }
 
 }
