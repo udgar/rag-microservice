6fef35ef6dceae86f13a29991b8450bd81c86346
udgarkhanal <khanaludgar@gmail.com>
2026-09-25 20:30:32 +0545

    add top 2 result generation from similarity search process to make prompt less verbose

--- src/main/java/com/udgar/rag/learning/config/RestClientConfiguration.java
+++ src/main/java/com/udgar/rag/learning/config/RestClientConfiguration.java
@@ -2,6 +2,7 @@ package com.udgar.rag.learning.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RestClientConfiguration {
 
+    // Here when Chat client is created first default question answer advisor is set, who retrieves the context of user's question from the vector store and add it to prompt
+    // Default system provides the default system prompt that is sent via all the prompt provided by the user
+    //search request is used to instruct to get top 2 similar search from the similarity search instead of all of them.
     @Bean
     ChatClient chatClient(ChatClient.Builder builder, VectorStore store) {
         return builder
                 .defaultSystem("You are a chat assistant that only answers questions, regarding the information stored in vector store," +
                         "if asked for anything else deny the request ")
-                .defaultAdvisors(QuestionAnswerAdvisor.builder(store).build())
+                .defaultAdvisors(QuestionAnswerAdvisor.builder(store).searchRequest(SearchRequest.builder().topK(2).build()).build())
                 .build();
     }
 
