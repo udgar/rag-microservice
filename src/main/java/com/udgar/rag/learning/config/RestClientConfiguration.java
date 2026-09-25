d01b1e061f39c6c978175fc2356760b17f00d45d
udgarkhanal <khanaludgar@gmail.com>
2026-09-25 15:21:15 +0545

    fix chatclient bean creation

--- src/main/java/com/udgar/rag/learning/config/RestClientConfiguration.java
+++ src/main/java/com/udgar/rag/learning/config/RestClientConfiguration.java
@@ -3,16 +3,14 @@ package com.udgar.rag.learning.config;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfiguration {
 
     @Bean
-    ChatClient chatClient(@Value("${outbound.llm.request.api}") String baseUrl, ChatClient.Builder builder, VectorStore store) {
+    ChatClient chatClient(ChatClient.Builder builder, VectorStore store) {
         return builder
                 .defaultSystem("You are a chat assistant that only answers questions, regarding the information stored in vector store," +
                         "if asked for anything else deny the request ")
